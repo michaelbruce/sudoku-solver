27821d3ea9c9a0fdcb3f62759219275a3ba44d61
Michael Bruce <michael.bruce@singletracksystems.com>
2016-12-13 01:10:00 +0000

    Rebirth - cut out lots of working out and restarted solve method

--- src/clj/sudoku_solver/core.clj
+++ src/clj/sudoku_solver/core.clj
@@ -1,4 +1,5 @@
-(ns sudoku-solver.core)
+(ns sudoku-solver.core
+  (:require [clojure.string :refer [index-of]]))
 
 ;; base02 - an incomplete sudoku puzzleboard
 (def puzzle
@@ -29,9 +30,6 @@
   "Returns an array of numbers missing from the argument array"
   (remove zero? (diff (flatten numlist) (range 1 10))))
 
-(defn take-at-point [point n col]
-  (take n (last (split-at point col))))
-
 (defn extract-grid [x y board]
   "Returns a 3x3 grid from a complete sudoku puzzleboard"
   (for [x (range x (+ x 3))
@@ -40,60 +38,6 @@
 
 (defn value-at-position [x y board]
   (-> (rows board) (nth y) (nth x)))
-
-(defn grid-coord [position]
-  (* (quot position 3) 3))
-
-(defn position-info [x y board]
-  [(nth (columns board) x)
-   (nth (rows board) y)
-   ;; extract-grid only works as expected with args reversed?
-   (extract-grid (grid-coord y) (grid-coord x) (rows board))
-   (value-at-position x y (rows board))])
-
-(defn possible-values
-  "Returns list of possible values for a given coordinate"
-  [x y board]
-  (missing
-    (distinct
-      (flatten
-        [(nth (columns board) x)
-         (nth (rows board) y)
-         (extract-grid (grid-coord y) (grid-coord x) (rows board))]))))
-
-;; stage 1 solver
-(for [y (range 0 9)
-      x (range 0 9)]
-  (let [value (value-at-position x y puzzle)]
-  (if (zero? value)
-    (possible-values x y puzzle)
-    value)))
-
-;; stage 2 solver
-(for [y (range 0 9)
-      x (range 0 9)]
-  (let [value (value-at-position x y puzzle)]
-    (let [pos (possible-values x y puzzle)]
-      (if (zero? value)
-        (if (= 1 (count pos))
-          (first pos) 0)
-        value))))
-
-(count '(6))
-
-;; solve by pos
-;; solve by row, then col?
-
-;; stage 1 solver
-(defn solve [board]
-  (for [y (range 0 9)
-        x (range 0 9)]
-    (let [value (value-at-position x y puzzle)]
-      (let [pos (possible-values x y puzzle)]
-        (if (zero? value)
-          (if (= 1 (count pos))
-            (first pos) 0)
-          value)))))
 
 (defn include? [value col]
   (not (nil? (some #{value} col))))
@@ -107,38 +51,35 @@
 
 (value-present 6 0 3 puzzle)
 
-(defn grid-solve [grid-x grid-y board]
-  "Examine each 3x3 grid, iterate over missing values in that grid and discover
-  coordinates that are valid for them by checking which related rows and columns contain a given value."
-  (value-present 6 grid-x grid-y board))
+;; Solving sudoku
+;; => take a puzzle as a vector
+;;   => take the top middle grid
+;;   => determine where 6 should be placed
+;;     => look at the top row, it contains 6 so it can't go there
+;;     => middle row doesn't have 6 - mark row 1
+;;     => bottom row has a 6 so it can't go there
+;;     => all columns are marked because they contain 6
+;; use the marked/unmarked results from the rows/columns to determine potential
+;; places to have 6
+;;  => for each row, is col position marked? is position 0/empty?
+;; [{:x 3 :y 2} {:x 3 :y 1}]
+;; (assoc puzzle (+ x (* y 9)) 6)
 
-;; middle right pos in top middle grid should be 6
-(grid-solve 0 3 puzzle)
+;; => will keep going until (indexOf puzzle 0) is -1
 
-;; top middle solve 6 pos
+;; (defn exists?)
 
-(defn solve [board]
-  "Organic or 'indirect' solving method programming ad-hoc ;)"
-  ;; First, find any easy pickings - columns with a single missing digit.
-  (let []))
+(defn solve [puzzle]
+  (let [grows (subvec (rows puzzle) 0 3)
+        gcols (subvec (columns puzzle) 3 6)]
+    (map #(include? 6 %) grows)))
+
+(solve puzzle)
+
+;; XXX working out
+;; (index-of (solve puzzle) true)
+(mapv #(index-of % 6) (subvec (rows puzzle) 0 3))
+(.indexOf (first (subvec (rows puzzle) 0 3)) 6)
 
 (defn -main [& args]
   (println "AHOY!"))
-
-;; ----------------------------------------------------------------------
-
-(map missing (columns puzzle)) ;; missing numbers in each column
-(map missing puzzle) ;; missing numbers in each row
-(map missing (for [x (range 0 9 3)
-                   y (range 0 9 3)]
-               (extract-grid x y puzzle))) ;; missing numbers in each grid
-
-(missing [[7 1 3 2 5] [7 4 3 2 9 8] [7 1 3 2 8]]) ;; results for point 0 0 only
-
-;; for each grid
-;;   - take the 3 columns that compose it
-;;   - take the 3 rows that compose it
-
-(for [x (range 0 9 3)
-      y (range 0 9 3)]
-  (extract-grid x y puzzle)) ;; all gris
